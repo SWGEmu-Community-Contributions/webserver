1115e00fe27243bc22b1b3508c2b56f7dd0fe6ca
Viaron <viaron@starwarsgalaxies.io>
2015-06-15 23:46:07 -0400

    [Added] Global.php and Phrases.php

---
+++ includes/global.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+SWGEmu Web Server Global Includes file.
+Please do not modify this file as it may
+alter the functionality or security of
+the web server.
+*/
+
+require 'config.php';
+require 'phrases.php';
+
+?>
---
+++ includes/phrases.php
@@ -0,0 +1,12 @@
+<?php
+// Placeholder
+
+/*
+This file will be used to add phrase variables to allow
+easy customization of various portions of the web server.
+
+The intention is that systems like "In-Game Mail" could easily
+be changed to use a different name like "Galactic Inbox" without
+needing to change multiple scripts or templates and instead just
+one master variable. Of course, default varibesl are included. 
+?>
